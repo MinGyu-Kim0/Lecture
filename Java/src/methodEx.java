536d8810a7eb5b41b1b2e7d0ca76bee7901fd60b
MINGYU KIM <coinmagic8085@gmail.com>
2025-09-03 22:20:53 +0900

    2025-09-03 리턴형을 사용해 값을 돌려주는 메소드 학습

--- Java/src/methodEx.java
+++ Java/src/methodEx.java
@@ -5,6 +5,19 @@
 //접근제한자 종류 : public protected default private
 //리턴형 종류 : 기본자료형, 객체형, void(리턴형이 없음을 의미)
 
+class MyCal {
+    //정수 덧셈 기능을 하는 메소드 구현
+    public int add(int x, int y) {
+        int r = x + y;
+        return r;
+    }
+    //실수 덧셈 기능을 하는 메소드 구현
+    public double add(double x, double y) {
+        double r = x + y;
+        return r;
+    }
+}
+
 class CookingRamen {
     //public(접근제한자) void(리턴형) 라면끓여줘(메소드이름)
     public void cook( int money ) { //메소드 정의
@@ -17,6 +30,14 @@ class CookingRamen {
 public class methodEx {
 
     public static void main(String[] args) {
+
+        MyCal myCal = new MyCal();
+        int result = myCal.add(1,2);
+        System.out.println(result);
+
+        double result2 = myCal.add(0.1,0.2);
+        System.out.println(result2);
+
         //라면끓여줘 메소드 사용(호출)
         //메소드 호출 : 메소드 이름을 사용하여 호출 가능
 
