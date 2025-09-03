18026c66a36415e7ca45430da9efdc1df5d5488a
MINGYU KIM <coinmagic8085@gmail.com>
2025-09-03 22:21:15 +0900

    2025-09-03 메소드를 사용해 중복되는 코드 제거 학습

--- Java/src/ScoreAverage.java
+++ Java/src/ScoreAverage.java
@@ -3,39 +3,71 @@
 //1. 모든 멤버변수 접근제한자 private
 
 class MyPerson {
-    int x1, x2, x3;
+    int age;
+    String name;
+    int score1;
+    int score2;
+    int score3;
+    int[] score = new int[10];
 
-    public void average() {
-        int sum = x1 + x2 + x3;
-
-        int average = sum / 3;
-
-        System.out.println(average);
+    public MyPerson(String n, int a, int s1, int s2, int s3) {
+        name = n;
+        age = a;
+        score1 = s1;
+        score2 = s2;
+        score3 = s3;
     }
+
+    public void 성적평균구하기() {
+        int result = score1 + score2 + score3;
+        e = result / 3;
+    }
+
+    public void 성적표출력() {
+        System.out.println("이름: " + name);
+        System.out.println("나이: " + age);
+        System.out.println("성적 평균: " + e);
+    }
+
 }
 
 public class ScoreAverage {
 
     public static void main(String[] args) {
-        MyPerson person = new MyPerson();
+        MyPerson m1 = new MyPerson("김길동", 20, 90, 80, 70);
+        //int result = m1.score1 + m1.score2 + m1.score3;
+        //int e = result / 3;
+        m1.성적평균구하기();
+        m1.성적표출력();
+//        System.out.println("이름: " + m1.name);
+//        System.out.println("나이: " + m1.age);
+//        System.out.println("성적 평균: " + m1.e);
 
-        person.x1 = 100;
-        person.x2 = 90;
-        person.x3 = 80;
+        MyPerson m2 = new MyPerson("박길동", 20, 100, 100, 90);
+//        m2.name = "박길동";
+//        m2.age = 20;
+//        m2.score1 = 100;
+//        m2.score2 = 100;
+//        m2.score3 = 90;
 
-        //평균 구하기 메소드 호출
-        person.average();
+//        int result2 = m2.score1 +  m2.score2 + m2.score3;
+//        int average2 = result2 / 3;
 
-        MyPerson person2 = new MyPerson();
+        m2.성적평균구하기();
+        m2.성적표출력();
+//        System.out.println("이름: " + m2.name);
+//        System.out.println("나이: " + m2.age);
+//        System.out.println("성적 평균: " + m2.e);
 
-        person2.x1 = 70;
-        person2.x2 = 80;
-        person2.x3 = 60;
+        MyPerson m3 = new MyPerson("홍길동", 22, 90, 80, 90);
+        m3.성적평균구하기();
+        m3.성적표출력();
 
-        person2.average();
+        //전체학생의 총점과 평균
+        int totalScore = m1.score1 + m2.score1 + m3.score2
+        +m2.score1 + m2.score2 + m2.score3 +
+                m3.score1 + m3.score2 + m3.score3;
 
-
-
-
+        int totalE = totalScore / 9;
     }
 }
